ee0484e86e55dde6f0c4ca41c85f0e95625c0f70
MaxBakshaev <risen.2011@mail.ru>
2024-10-22 01:32:47 +0900

    функция plati() разбита на 4 функции

--- src/functions/platimarket.py
+++ src/functions/platimarket.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 
 
 def plati(game_name, dict_price_url):
+# Получение информации о ключах игр из API сайта platimarket.ru
 
     # количество продавцов на страницу
     pagesize = 10
@@ -30,7 +31,7 @@ def plati(game_name, dict_price_url):
     ]
 
     # API поиск игр
-    url_plati = (f'https://plati.io/api/search.ashx?query={game_name}&pagesize={pagesize}&pagenum={pagenum}&visibleOnly=True&response=json')
+    url_plati = f'https://plati.io/api/search.ashx?query={game_name}&pagesize={pagesize}&pagenum={pagenum}&visibleOnly=True&response=json'
 
     try:
         response_plati = requests.get(url_plati)
@@ -41,92 +42,119 @@ def plati(game_name, dict_price_url):
         repo_dicts = response_dict['items']
 
         # Проверка продавцов из списка на надежность
-        for repo_dict in repo_dicts:
-            try:
-                # Строка 'name' с названием игры на сайте
-                full_name_in_site = repo_dict['name']
-                name_in_site = full_name_in_site.lower()
-
-                # Проверка названия игры
-                if game_name not in name_in_site:
-                    raise Exception
-
-                # Проверка слов исключений для названия
-                if any(word in name_in_site for word in banwords_name):
-                    raise Exception
-
-                # Проверка слов исключений для описания
-                description = repo_dict['description'].lower()
-                if any(word_des in description for word_des in
-                       banwords_description):
-                    raise Exception
-
-                # Проверка платформы Steam
-                if 'steam' not in name_in_site:
-                    if 'steam' not in description:
-                        raise Exception
-
-                # Рейтинг продавца
-                if repo_dict['seller_rating'] < 100:
-                    raise Exception
-
-                # Количество продаж
-                sold = repo_dict['numsold']
-                if sold < 30:
-                    raise Exception
-
-                # Количество возвратов
-                returns = repo_dict['count_returns']
-                if returns > 5:
-                    raise Exception
-
-                # Соотношение возвратов к продажам
-                if returns / sold > 0.01:
-                    raise Exception
-
-                # Количество позитивных отзывов
-                positiveresponses = repo_dict['count_positiveresponses']
-                if positiveresponses < 5:
-                    raise Exception
-
-                # Количество негативных отзывов
-                negativeresponses = repo_dict['count_negativeresponses']
-                if negativeresponses > 5:
-                    raise Exception
-
-                # Соотношение негативных отзывов к позитивным
-                if negativeresponses / positiveresponses > 0.01:
-                    raise Exception
-
-                # Проверка на наличие ключа в продаже
-                try:
-                    request_site = repo_dict['url']
-                    response_site = requests.get(request_site)
-                    response_site.raise_for_status()
-                    soup_plati = BeautifulSoup(
-                        response_site.text, 'lxml')
-
-                    # Если есть этот тег, то товар не в продаже
-                    key_in_site = soup_plati.find(
-                        'div', class_='goods_order_form_subscribe')
-                    if key_in_site is None:
-
-                        # Формирование цены игры и ссылки
-                        repo_price = repo_dict['price_rur']
-                        repo_url = repo_dict['url']
-
-                        # Цена игры и ссылка добавляются в словарь
-                        dict_price_url[repo_price] = (
-                            repo_url, full_name_in_site)
-
-                except Exception:
-                    raise Exception
-
-            except Exception:
-                continue
+        check_reliability(
+            dict_price_url, repo_dicts, banwords_name, banwords_description,
+            game_name)
 
     except Exception:
         pass
+
+
+def check_reliability(
+        dict_price_url, repo_dicts, banwords_name, banwords_description,
+        game_name):
+    # Проверка продавцов из списка на надежность
+    for repo_dict in repo_dicts:
+        try:
+            # cтрока 'name' с названием игры на сайте
+            full_name_in_site = repo_dict['name']
+            name_in_site = full_name_in_site.lower()
+
+            # Проверка надежности продавца по разным фильтрам
+            check_different_filters(
+                game_name, name_in_site, repo_dict, banwords_name,
+                banwords_description)
+
+            # ссылка на игру
+            repo_url = repo_dict['url']
+
+            # Проверка на наличие ключа в продаже
+            check_key_in_stock(repo_url)
+
+            # цена игры
+            repo_price = repo_dict['price_rur']
+
+            # Если проходит проверку на надежность, то
+            # цена игры и ссылка добавляются в словарь
+            dict_price_url[repo_price] = repo_url, full_name_in_site
+
+        except Exception:
+            continue
+
+
+def check_different_filters(
+        game_name, name_in_site, repo_dict, banwords_name,
+        banwords_description):
+    # Проверка надежности продавца по разным фильтрам
+
+    # Проверка названия игры
+    if game_name not in name_in_site:
+        raise Exception
+
+    # Проверка слов исключений для названия
+    if any(word in name_in_site for word in banwords_name):
+        raise Exception
+
+    # Проверка слов исключений для описания
+    description = repo_dict['description'].lower()
+    if any(word_des in description for word_des in
+           banwords_description):
+        raise Exception
+
+    # Проверка платформы Steam
+    if 'steam' not in name_in_site:
+        if 'steam' not in description:
+            raise Exception
+
+    # Рейтинг продавца
+    if repo_dict['seller_rating'] < 100:
+        raise Exception
+
+    # Количество продаж
+    sold = repo_dict['numsold']
+    if sold < 30:
+        raise Exception
+
+    # Количество возвратов
+    returns = repo_dict['count_returns']
+    if returns > 5:
+        raise Exception
+
+    # Соотношение возвратов к продажам
+    if returns / sold > 0.01:
+        raise Exception
+
+    # Количество позитивных отзывов
+    positiveresponses = repo_dict['count_positiveresponses']
+    if positiveresponses < 5:
+        raise Exception
+
+    # Количество негативных отзывов
+    negativeresponses = repo_dict['count_negativeresponses']
+    if negativeresponses > 5:
+        raise Exception
+
+    # Соотношение негативных отзывов к позитивным
+    if negativeresponses / positiveresponses > 0.01:
+        raise Exception
+
+
+def check_key_in_stock(repo_url):
+    # Проверка на наличие ключа в продаже
+    try:
+        response_site = requests.get(repo_url)
+        response_site.raise_for_status()
+        soup_plati = BeautifulSoup(
+            response_site.text, 'lxml')
+
+        # Если есть этот тег, то товар не в продаже
+        key_in_site = soup_plati.find(
+            'div', class_='goods_order_form_subscribe')
+        if key_in_site is not None:
+            raise Exception
+
+    except Exception:
+        raise Exception
 
 
 if __name__ == '__main__':
