6b52fd8a17a2784caf71ca0418f55f75ed527c32
MaxBakshaev <risen.2011@mail.ru>
2024-10-22 02:46:05 +0900

    функция steam_pay разбита на 4 функции

--- src/functions/steampay.py
+++ src/functions/steampay.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 
 
 def steam_pay(game_name, dict_price_url):
+# Получение информации о ключе игры с сайта steampay.com
 
     # Формирование ссылки для перехода на страницу игры
     game_name_link = '-'.join(list(game_name.split(' ')))
@@ -15,40 +16,53 @@ def steam_pay(game_name, dict_price_url):
         soup_steampay = BeautifulSoup(response_steampay.text, 'lxml')
 
         # Проверка на наличие ключа в продаже
-        key_in_stock = soup_steampay.find(
-            'span', class_='product__advantages-orange')
-        stock = str(key_in_stock)
-        if 'закончился' in stock:
-            raise Exception
-        if 'ожидается' in stock:
-            raise Exception
+        check_key_in_stock(soup_steampay)
 
         # Проверка региона Росссия
-        region = soup_steampay.find_all(
-            'ul', class_='product__info-inner-block-list')
-        sell_region = str(region[3])
-        if 'Россия' not in sell_region:
-            raise Exception
+        check_ru_region(soup_steampay)
 
-        # Формирование цены игры
-        game_name_steampay = soup_steampay.find(
-            'h1', class_='product__title')
-        name_steampay = game_name_steampay.text.strip()
-        line = str(soup_steampay.find(
-            'div', class_='product__current-price'))
-        line_in_list = line.split(' ')
-        element = line_in_list[1]
-        price_in_element = ''.join([x for x in element if x.isdigit()])
-
-        # Цена игры и ссылка добавляются в словарь
-        dict_price_url[int(price_in_element)] = url_steampay, name_steampay
+        # Формирование цены игры и добавление в словарь
+        gameprice_form(dict_price_url, soup_steampay, url_steampay)
 
     except Exception:
         pass
 
 
+def check_key_in_stock(soup_steampay):
+    # Проверка на наличие ключа в продаже
+    key_in_stock = soup_steampay.find(
+        'span', class_='product__advantages-orange')
+    stock = str(key_in_stock)
+    if 'закончился' in stock:
+        raise Exception
+    if 'ожидается' in stock:
+        raise Exception
+
+
+def check_ru_region(soup_steampay):
+    # Проверка региона Росссия
+    region = soup_steampay.find_all(
+        'ul', class_='product__info-inner-block-list')
+    sell_region = str(region[3])
+    if 'Россия' not in sell_region:
+        raise Exception
+
+
+def gameprice_form(dict_price_url, soup_steampay, url_steampay):
+    # Формирование цены игры
+    game_name_steampay = soup_steampay.find('h1', class_='product__title')
+    name_steampay = game_name_steampay.text.strip()
+    line = str(soup_steampay.find('div', class_='product__current-price'))
+    line_in_list = line.split(' ')
+    element = line_in_list[1]
+    price_in_element = ''.join([x for x in element if x.isdigit()])
+
+    # Цена игры и ссылка добавляются в словарь
+    dict_price_url[int(price_in_element)] = url_steampay, name_steampay
+
+
 if __name__ == '__main__':
-    game_name = input('Введите название игры: ').lower()
+    game_name = input('Введите название игры: ').lower() # напр. Superliminal
     dict_price_url = {}
     steam_pay(game_name, dict_price_url)
     print(dict_price_url)
